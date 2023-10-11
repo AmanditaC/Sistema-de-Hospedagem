cc580473ff29fb25c6912a52f6388326d5d7b569
AmanditaC <amandacristinagomes8@gamail.com>
2023-10-10 23:17:25 -0300

    começo da classe reserva

--- src/Reserva.java
+++ src/Reserva.java
@@ -1,4 +1,27 @@
 public class Reserva {
     private Hospede hospede;
     private Quarto quarto;
+    
+    public Reserva(Hospede hospede, Quarto quarto) {
+        this.hospede = hospede;
+        this.quarto = quarto;
+    }
+
+    public Hospede getHospede() {
+        return hospede;
+    }
+
+    public void setHospede(Hospede hospede) {
+        this.hospede = hospede;
+    }
+
+    public Quarto getQuarto() {
+        return quarto;
+    }
+
+    public void setQuarto(Quarto quarto) {
+        this.quarto = quarto;
+    }
+
+    
 }
